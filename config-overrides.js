b942d9cefc4f83d30e0cdf2d2b092323054f8d1f
detanx <detanit@163.com>
2018-12-19 18:21:45 +0800

    update dataType

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-
-module.exports = function override(config, env) {
-    config = injectBabelPlugin(
-        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-        config,
-    );
-    return config;
-};
